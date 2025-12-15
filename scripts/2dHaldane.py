a9b2693f94033ecd4c60a47e8397f24db683a584
Syl Shaw <sylshaw01@gmail.com>
2025-12-15 23:23:30 +0000

    add scripts for Haldane

--- scripts/2dHaldane.py
+++ scripts/2dHaldane.py
@@ -21,13 +21,13 @@ from SLmodels import *
 
 def single_iteration(args):
     # Unpack arguments
-    L, rho, kappa, disorder, num_eigval, X, sparse,return_evec, return_eval, i = args
+    L, rho, kappa, disorder, num_eigval, X, sparse,return_evec, return_eval, t1, t2, M, phi, i = args
     # Generate unique seed for reproducibility, using hashlib to avoid collisions
-    seed_str = f"{rho}_{disorder}_{num_eigval}_{i}_{kappa:.5f}_{L}_{v}_{w}"
+    seed_str = f"{rho}_{disorder}_{num_eigval}_{i}_{kappa:.5f}_{L}"
     #seed = int(rho * 10e5) + int(disorder * 10e7) + int(num_eigval*10e4) + i OLD SEED GENERATION METHOD
     seed = int(hashlib.md5(seed_str.encode()).hexdigest(),16) % (2**32)
     np.random.seed(seed)
-    m = ThreeDimensionalAnderson(L, disorder, rho, kappa,X)
+    m = TwoDimensionalHaldane(L, disorder, rho, kappa,X, t1, t2, M, phi)
     m.find_eigval(m.H, num_eigval=num_eigval, sparse=sparse)
     H_eigval = m.H_eigval
     H_eigvec = m.H_eigvec
@@ -36,21 +36,22 @@ def single_iteration(args):
     spectral_localiser_eigvec = m.spectral_localiser_eigvec
     spectral_localiser_IPR = m.compute_IPR(m.spectral_localiser_eigvec)
     H_IPR = m.compute_IPR(m.H_eigvec)
+    chern_BR = m.calculate_local_chern_marker()
     if retevec:
-        return  H_eigval, spectral_localiser_eigval, H_eigvec, spectral_localiser_eigvec, seed
-    return  H_eigval, spectral_localiser_eigval,H_IPR, spectral_localiser_IPR,  seed
+        return  H_eigval, spectral_localiser_eigval, H_eigvec, spectral_localiser_eigvec, chern_BR, seed
+    return  H_eigval, spectral_localiser_eigval,H_IPR, spectral_localiser_IPR, chern_BR,  seed
 
 
 
 if __name__ == "__main__":
     # Take cpu count and parameters file from command line arguments
     cpu_count = int(sys.argv[1]) if len(sys.argv) > 1 else cpu_count()
-    parameters_file = sys.argv[2] if len(sys.argv) > 2 else 'parameters_3dAnderson.txt'
+    parameters_file = sys.argv[2] if len(sys.argv) > 2 else 'parameters_2dHaldane.txt'
 
     
 
     print("-"*50)
-    print("Calculating 3D Anderson model Hamiltonian and spectral localiser statistics")
+    print("Calculating 2D Haldane model Hamiltonian and spectral localiser statistics")
     print("-"*50)
 
     parameters = {}
@@ -66,6 +67,12 @@ if __name__ == "__main__":
 
     rho = float(parameters.get('rho', 30.0))
     kappa = float(parameters.get('kappa', 0.1))
+    v = float(parameters.get('v', 1.0))
+    w = float(parameters.get('w', 1.5))
+    t1 = float(parameters.get('t1', 1.0))
+    t2 = float(parameters.get('t2', 1.0/3.0))
+    M = float(parameters.get('M', 1.0))
+    phi = float(parameters.get('phi', np.pi/2))
     disorder_start = float(parameters.get('disorder_start', 0.0))
     disorder_end = float(parameters.get('disorder_end', 5.0))
     disorder_resolution = int(parameters.get('disorder_resolution', 6))
@@ -80,12 +87,13 @@ if __name__ == "__main__":
     retevec = False
 
     current_date = datetime.datetime.now().strftime("%Y-%m-%d-%H%M%S")
-    base_name = f"../data/3dAnderson_L{L}_w_{w}_disorder{disorder_start}-{disorder_end}_numEigs{num_eigenvalues}_realizations{num_disorder_realisations}_{current_date}"
+    base_name = f"../data/2dHaldane_L{L}_w_{w}_disorder{disorder_start}-{disorder_end}_numEigs{num_eigenvalues}_realizations{num_disorder_realisations}_{current_date}"
 
-    shape_4d_H = (len(disorder_values), num_disorder_realisations, L**3, L**3)
-    shape_4d_SL = (len(disorder_values), num_disorder_realisations, 4*L**3, 4*L**3)
-    shape_3d_H = (len(disorder_values), num_disorder_realisations, L**3)
-    shape_3d_SL = (len(disorder_values), num_disorder_realisations, 4*L**3)
+    shape_4d_H = (len(disorder_values), num_disorder_realisations, 2*L**2, 2*L**2)
+    shape_4d_SL = (len(disorder_values), num_disorder_realisations, 4*L**2, 4*L**2)
+    shape_3d_H = (len(disorder_values), num_disorder_realisations, 2*L**2)
+    shape_3d_SL = (len(disorder_values), num_disorder_realisations, 4*L**2)
+    shape_4d_chern = (len(disorder_values), num_disorder_realisations, L, L)    
     shape_2d = (len(disorder_values), num_disorder_realisations)
 
 
@@ -103,6 +111,7 @@ if __name__ == "__main__":
     spectral_localiser_eigval_results = np.memmap(f"{base_name}_spectral_localiser_eigval.dat", dtype='float64', mode='w+', shape=shape_3d_SL)
     H_IPR_results = np.memmap(f"{base_name}_H_IPR.dat", dtype='float64', mode='w+', shape=shape_3d_H)
     spectral_localiser_IPR_results = np.memmap(f"{base_name}_spectral_localiser_IPR.dat", dtype='float64', mode='w+', shape=shape_3d_SL)
+    H_chern_BR_results = np.memmap(f"{base_name}_H_chern_BR.dat", dtype='float64', mode='w+', shape=shape_4d_chern)
     if retevec:
         H_eigvec_results = np.memmap(f"{base_name}_H_eigvec.dat", dtype='complex128', mode='w+', shape=shape_4d_H)
         spectral_localiser_eigvec_results = np.memmap(f"{base_name}_spectral_localiser_eigvec.dat", dtype='complex128', mode='w+', shape=shape_4d_SL)
@@ -116,51 +125,51 @@ if __name__ == "__main__":
             f.write(f"{key} = {value}\n")
             f.write(f"Created at {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
 
-
-
+    
 
     with Pool(processes=cpu_count, maxtasksperchild=10) as pool:
-        modelToGetX =  ThreeDimensionalAnderson(L,0,rho,1.0)
+        modelToGetX =  TwoDimensionalHaldane(L,0,rho,kappa,t1=1.0,t2=1.0/3.0,M=1.0,phi=np.pi/2)
         X = modelToGetX.X
         sparse = False
         num_eigval = num_eigenvalues
         for j, disorder in enumerate(disorder_values):
             print(f"   Disorder: {disorder}", flush=True)
             disorder_time = time.time()
-            modelforkappa = ThreeDimensionalAnderson(L,disorder,rho=L//2,kappa=1.0)
-            #Find largest eigenvalue to set kappa appropriately
-            largest_eigenvalue = eigsh(modelforkappa.H, k=1, which='LM', return_eigenvectors=False)[0]
-            kappa = largest_eigenvalue
-            args_list  = [(L, rho, kappa, disorder, num_eigval, X, sparse,retevec, reteval, i) for i in range(num_disorder_realisations)]
+            kappa = (3 + disorder * 0.5)/ rho # Rough value which is appropriate for kappa, for the Haldane model
+            args_list  = [(L, rho, kappa, disorder, num_eigval, X, sparse,retevec, reteval,t1, t2, M, phi, i) for i in range(num_disorder_realisations)]
             results = list(pool.imap(single_iteration, args_list, chunksize=1))
             print(f"      Time for disorder {disorder}: {time.time() - disorder_time} seconds", flush=True)
             if retevec:
-                H_eigval, spectral_localiser_eigval, H_eigvec, spectral_localiser_eigvec, seed_values = zip(*results)
+                H_eigval, spectral_localiser_eigval, H_eigvec, spectral_localiser_eigvec, H_chern_BR, seed_values = zip(*results)
                 seeds[ j, :] = seed_values
                 H_eigval_results[j,:, :] = np.array(H_eigval)
                 spectral_localiser_eigval_results[j,:, :] = np.array(spectral_localiser_eigval)
                 H_eigvec_results[j,:, :, :] = np.array(H_eigvec)
                 spectral_localiser_eigvec_results[j,:, :, :] = np.array(spectral_localiser_eigvec)
+                H_chern_BR_results[j,:, :] = np.array(H_chern_BR)
 
                 seeds.flush()
                 H_eigval_results.flush()
                 spectral_localiser_eigval_results.flush()
                 H_eigvec_results.flush()
                 spectral_localiser_eigvec_results.flush()
+                H_chern_BR_results.flush()
                 continue
             else:
-                H_eigval, spectral_localiser_eigval, H_IPR, spectral_localiser_IPR, seed_values = zip(*results)
+                H_eigval, spectral_localiser_eigval, H_IPR, spectral_localiser_IPR,H_chern_BR, seed_values = zip(*results)
                 seeds[ j, :] = seed_values
                 H_eigval_results[j,:, :] = np.array(H_eigval)
                 spectral_localiser_eigval_results[j,:, :] = np.array(spectral_localiser_eigval)
                 H_IPR_results[j,:, :] = np.array(H_IPR)
                 spectral_localiser_IPR_results[j,:, :] = np.array(spectral_localiser_IPR)
+                H_chern_BR_results[j,:, :] = np.array(H_chern_BR)
 
                 seeds.flush()
                 H_eigval_results.flush()
                 spectral_localiser_eigval_results.flush()
                 H_IPR_results.flush()
                 spectral_localiser_IPR_results.flush()
+                H_chern_BR_results.flush()
 
     elapsed_time = time.time() - total_time
     print(f"Total time for all calculations: {elapsed_time:.2f} seconds, or {(elapsed_time)/60:.2f} minutes, {(elapsed_time)/3600:.2f} hours", flush=True)   
---
+++ scripts/2dHaldane.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+
+cat > job2dHaldane.sh << "EOF"
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=84
+#SBATCH --mem-per-cpu=4591
+#SBATCH --time=48:00:00
+
+
+module purge
+module load GCC/13.2.0 OpenMPI/4.1.6 SciPy-bundle/2023.11
+
+export p=$SLURM_CPUS_PER_TASK
+export SLURM_CPU_BIND=none
+export OMP_NUM_THREADS=1
+
+
+# export INSTALL_DIR="$HOME/slepc-gnu-install"
+# export PETSC_DIR="$INSTALL_DIR/petsc-main"
+# export PETSC_ARCH="arch-linux-c-opt"
+# export SLEPC_DIR="$INSTALL_DIR/slepc-main"
+
+
+# source "$INSTALL_DIR/slepc_env/bin/activate"
+
+
+
+
+
+
+python 2dHaldane_combined.py $p "$1"
+EOF
+
+python - <<"EOF"
+import re
+import sys
+
+# read SLModels
+with open("../src/SLmodels.py", "r") as f:
+    slmodels_code = f.read()
+
+# read 2dHaldane
+with open("2dHaldane.py", "r") as f:
+    SSH_code = f.read()
+
+Haldane_code = re.sub(r"^sys\.path\.append\(['\"]\.\.\/src['\"]\)$", "", SSH_code, flags=re.MULTILINE)
+
+Haldane_code = re.sub(r"^from SLmodels import \*$", "", SSH_code, flags=re.MULTILINE)
+
+
+combined = f"""#!/usr/bin/env python3
+
+{slmodels_code}
+
+{Haldane_code}
+"""
+
+with open("2dHaldane_combined.py", "w") as f:
+    f.write(combined)
+EOF
+
+sbatch job2dHaldane.sh $1
+
+
+
+
