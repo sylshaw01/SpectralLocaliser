042502581b41e463122e1f8599d10b46dc63ce15
Syl Shaw <sylshaw01@gmail.com>
2026-01-08 18:20:25 +0000

    testing kappa with topological transition

---
+++ scripts/1dSSH-kappa-test-topological-transition.py
@@ -0,0 +1,191 @@
+
+# Set environment variables to make sure pool behaves! otherwise I think it ends up locking itself by assigning too many threads
+import os
+os.environ['OMP_NUM_THREADS'] = '1'
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
+os.environ['MKL_NUM_THREADS'] = '1'
+os.environ['VECLIB_MAXIMUM_THREADS'] = '1'
+os.environ['NUMEXPR_NUM_THREADS'] = '1'
+
+# Normal imports
+import numpy as np
+from multiprocessing import cpu_count
+from multiprocessing import Pool
+import time
+import datetime
+import sys
+import hashlib
+# Import SLmodels from src
+sys.path.append('../src')
+from SLmodels import *
+
+def single_iteration(args):
+    # Unpack arguments
+    L, rho, kappa, disorder, num_eigval, X, sparse,return_evec, return_eval, v,w, i = args
+    # Generate unique seed for reproducibility, using hashlib to avoid collisions
+    time_of_day = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
+    seed_str = f"{rho}_{disorder}_{num_eigval}_{i}_{kappa:.5f}_{L}_{v}_{w}_{time_of_day}"
+    #seed = int(rho * 10e5) + int(disorder * 10e7) + int(num_eigval*10e4) + i OLD SEED GENERATION METHOD
+    seed = int(hashlib.md5(seed_str.encode()).hexdigest(),16) % (2**32)
+    np.random.seed(seed)
+    m = OneDimensionalSSHBlockBasis(L, disorder, rho, kappa,v,w,X)
+    m.find_eigval(m.H, num_eigval=num_eigval, sparse=sparse)
+    H_eigval = m.H_eigval
+    H_eigvec = m.H_eigvec
+    m.find_eigval(m.spectral_localiser, num_eigval=num_eigval, sparse=sparse)
+    spectral_localiser_eigval = m.spectral_localiser_eigval
+    spectral_localiser_eigvec = m.spectral_localiser_eigvec
+    spectral_localiser_IPR = m.compute_IPR(m.spectral_localiser_eigvec)
+    H_IPR = m.compute_IPR(m.H_eigvec)
+    if retevec:
+        return  H_eigval, spectral_localiser_eigval, H_eigvec, spectral_localiser_eigvec, seed
+    return  H_eigval, spectral_localiser_eigval,H_IPR, spectral_localiser_IPR,  seed
+
+
+
+if __name__ == "__main__":
+    # Take cpu count and parameters file from command line arguments
+    cpu_count = int(sys.argv[1]) if len(sys.argv) > 1 else cpu_count()
+    parameters_file = sys.argv[2] if len(sys.argv) > 2 else 'parameters_1dSSH.txt'
+
+    
+
+    print("-"*50)
+    print("Calculating 1D SSH model Hamiltonian and spectral localiser statistics")
+    print("-"*50)
+
+    parameters = {}
+    with open(parameters_file, 'r') as f:
+        for line in f:
+            if line.strip() and not line.startswith('#'):
+                key, value = line.split('=')
+                parameters[key.strip()] = value.strip()
+
+    # Import parameters from file
+    L = int(parameters.get('L', 200))
+    num_disorder_realisations = int(parameters.get('num_disorder_realisations', 100))
+
+    rho = float(parameters.get('rho', 30.0))
+    kappa_file = float(parameters.get('kappa', 0.1)) # IN THIS VERSION ONLY, KAPPA BECOMES SUCH THAT KAPPA * RHO = "kappa" *  |H|
+    v = float(parameters.get('v', 1.0))
+    w_start = float(parameters.get('w_start', 0.5))
+    w_end = float(parameters.get('w_end', 1.5))
+    w_resolution = int(parameters.get('w_resolution', 50))
+    disorder = float(parameters.get('disorder', 2.0))
+    # disorder_start = float(parameters.get('disorder_start', 0.0))
+    # disorder_end = float(parameters.get('disorder_end', 5.0))
+    # disorder_resolution = int(parameters.get('disorder_resolution', 6))
+    # Number of eigenvalues to compute if we are using sparse methods
+    num_eigenvalues = int(parameters.get('num_eigenvalues', 600))
+
+
+    # disorder_values = np.linspace(disorder_start, disorder_end, disorder_resolution)
+    w_values = np.linspace(w_start, w_end, w_resolution)
+
+    # Boolean flags to get eigenvalues and eigenvectors
+    reteval = True
+    retevec = False
+
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d-%H%M%S")
+    base_name = f"../data/1dSSH_L{L}_w_{w_start}-{w_end}_{kappa_file}_disorder{disorder}-_numEigs{num_eigenvalues}_realizations{num_disorder_realisations}_{current_date}"
+
+    shape_4d = (len(w_values), num_disorder_realisations, L, L)
+    shape_3d = (len(w_values), num_disorder_realisations, L)
+    shape_2d = (len(w_values), num_disorder_realisations)
+
+
+
+    print(f"{cpu_count} Cores found! Using...All of them!!")
+    print(f"Parameters loaded from {parameters_file}")
+    print(f"w from {w_start} to {w_end} with {disorder} steps")
+    print(f"{num_disorder_realisations} disorder realizations per parameter set")
+    print(f"Disorder strength: {disorder}")
+    print(f"kappa scaling factor: {kappa_file}")
+    print(f"Calculating {num_eigenvalues} eigenvalues per run")
+    print("-"*50, flush=True)
+
+
+    # NOTE - If sparse is true, then the size of these arrays needs to change
+    H_eigval_results = np.memmap(f"{base_name}_H_eigval.dat", dtype='float64', mode='w+', shape=shape_3d)
+    spectral_localiser_eigval_results = np.memmap(f"{base_name}_spectral_localiser_eigval.dat", dtype='float64', mode='w+', shape=shape_3d)
+    H_IPR_results = np.memmap(f"{base_name}_H_IPR.dat", dtype='float64', mode='w+', shape=shape_3d)
+    spectral_localiser_IPR_results = np.memmap(f"{base_name}_spectral_localiser_IPR.dat", dtype='float64', mode='w+', shape=shape_3d)
+    if retevec:
+        H_eigvec_results = np.memmap(f"{base_name}_H_eigvec.dat", dtype='complex128', mode='w+', shape=shape_4d)
+        spectral_localiser_eigvec_results = np.memmap(f"{base_name}_spectral_localiser_eigvec.dat", dtype='complex128', mode='w+', shape=shape_4d)
+    
+    seeds = np.memmap(f"{base_name}_seeds.dat", dtype='int64', mode='w+', shape=shape_2d)
+    total_time = time.time()
+
+
+    with open(f"{base_name}_parameters.txt", 'w') as f:
+        for key, value in parameters.items():
+            f.write(f"{key} = {value}\n")
+            f.write(f"Created at {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+
+
+
+
+    with Pool(processes=cpu_count, maxtasksperchild=10) as pool:
+        modelToGetX =  OneDimensionalSSHBlockBasis(L,0,rho,1,v,w_start)
+        X = modelToGetX.X
+        sparse = False
+        num_eigval = num_eigenvalues
+        for j, w in enumerate(w_values):
+            print(f"   w: {w}", flush=True)
+            disorder_time = time.time()
+            modelforkappa = OneDimensionalSSHBlockBasis(L, disorder, rho, 1,v,w)
+            largest_eigenvalue = eigsh(modelforkappa.H, k=1, which='LM', return_eigenvectors=False)[0]
+            kappa = kappa_file * (largest_eigenvalue/ rho) # Rough value which is appropriate for kappa, for the SSH model
+            print(f"      |H| is approximately {largest_eigenvalue:.2f}", flush=True)
+            print(f"      Using kappa = {kappa:.10f}", flush=True)
+            args_list  = [(L, rho, kappa, disorder, num_eigval, X, sparse, retevec, reteval, v,w, i) for i in range(num_disorder_realisations)]
+            results = list(pool.imap(single_iteration, args_list, chunksize=1))
+            print(f"      Time for w {w}: {time.time() - disorder_time} seconds", flush=True)
+            if retevec:
+                H_eigval, spectral_localiser_eigval, H_eigvec, spectral_localiser_eigvec, H_IPR, spectral_localiser_IPR, seed_values = zip(*results)
+                seeds[ j, :] = seed_values
+                H_eigval_results[j,:, :] = np.array(H_eigval)
+                spectral_localiser_eigval_results[j,:, :] = np.array(spectral_localiser_eigval)
+                H_eigvec_results[j,:, :, :] = np.array(H_eigvec)
+                spectral_localiser_eigvec_results[j,:, :, :] = np.array(spectral_localiser_eigvec)
+                H_IPR_results[j,:, :] = np.array(H_IPR)
+                spectral_localiser_IPR_results[j,:, :] = np.array(spectral_localiser_IPR)
+
+
+                seeds.flush()
+                H_eigval_results.flush()
+                spectral_localiser_eigval_results.flush()
+                H_eigvec_results.flush()
+                spectral_localiser_eigvec_results.flush()
+                H_IPR_results.flush()
+                spectral_localiser_IPR_results.flush()
+                continue
+            else:
+                H_eigval, spectral_localiser_eigval, H_IPR, spectral_localiser_IPR, seed_values = zip(*results)
+                seeds[ j, :] = seed_values
+                H_eigval_results[j,:, :] = np.array(H_eigval)
+                spectral_localiser_eigval_results[j,:, :] = np.array(spectral_localiser_eigval)
+                H_IPR_results[j,:, :] = np.array(H_IPR)
+                spectral_localiser_IPR_results[j,:, :] = np.array(spectral_localiser_IPR)
+
+                seeds.flush()
+                H_eigval_results.flush()
+                spectral_localiser_eigval_results.flush()
+                H_IPR_results.flush()
+                spectral_localiser_IPR_results.flush()
+
+    elapsed_time = time.time() - total_time
+    print(f"Total time for all calculations: {elapsed_time:.2f} seconds, or {(elapsed_time)/60:.2f} minutes, {(elapsed_time)/3600:.2f} hours", flush=True)   
+    
+
+
+    
+    # np.savez(filename, disorder_values = disorder_values, seeds = seeds, H_eigval = H_eigval_results, spectral_localiser_eigval=spectral_localiser_eigval_results, H_IPR=H_IPR_results, spectral_localiser_IPR=spectral_localiser_IPR_results)   
+    # print(f"Results saved to {filename}", flush=True)
+
+
+
+
+
+
